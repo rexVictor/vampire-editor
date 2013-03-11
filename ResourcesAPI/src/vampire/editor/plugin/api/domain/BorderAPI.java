d41fa14da20390f48577133f91eed01cd8236437
development.rex <development.rex@gmail.com>
2013-03-11 15:41:50 +0000

    Added license header and javadoc

--- ResourcesAPI/src/vampire/editor/plugin/api/domain/BorderAPI.java
+++ ResourcesAPI/src/vampire/editor/plugin/api/domain/BorderAPI.java
@@ -1,21 +1,49 @@
+/*******************************************************************************
+ * Vampire Editor Resources API.
+ * Copyright (C) 2013  Matthias Johannes Reimchen
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Further information can be obtained at
+ * https://code.google.com/p/vampire-editor/ or via mail:
+ * Matthias Johannes Reimchen
+ * development.rex@gmail.com
+ ******************************************************************************/
 package vampire.editor.plugin.api.domain;
 
 import java.awt.Image;
 
+/**
+ * The interface providing read-access to a border object. <br>
+ * It stores an {@link Image} and the sizes in milli meters.
+ * @author rex_victor
+ *
+ */
 public interface BorderAPI {
 
-	public abstract Image getImage();
+	public Image getImage();
 
-	public abstract int getLeftInset();
+	public int getLeftInset();
 
-	public abstract int getRightInset();
+	public int getRightInset();
 
-	public abstract int getTopInset();
+	public int getTopInset();
 
-	public abstract int getBottomInset();
+	public int getBottomInset();
 
-	public abstract int getSheetWidth();
+	public int getSheetWidth();
 
-	public abstract int getSheetHeight();
+	public int getSheetHeight();
 
 }
